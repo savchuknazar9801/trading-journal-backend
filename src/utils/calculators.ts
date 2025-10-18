8ee04c586c4b7e6b46a9c53ac1e33ae560dc333c
darkhorse00512 <horsedark374@gmail.com>
2025-10-18 04:53:35 -0400

    fix: up and down limit error

--- src/utils/calculators.ts
+++ src/utils/calculators.ts
@@ -4,6 +4,9 @@ export const calculateHoldingTime = (entryDate: string, exitDate: string) => {
     const entry = new Date(entryDate);
     const exit = new Date(exitDate);
 
+    // Calculate differences in milliseconds 
+    const diffInMs = exit.getTime() - entry.getTime();
+
     // Convert to minutes
     const diffInMinutes = diffInMs / (1000 * 60);
 
